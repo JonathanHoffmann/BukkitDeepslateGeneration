e06fbd082ee3437c4b3635a3771ec4086353160f
Jonathan Hoffmann <jonathan.hoffmann@gmx.net>
2023-10-13 21:58:47 +0200

    v2

--- src/main/java/DeepslateGeneration/DeepslateGeneration/BlockFormListener.java
+++ src/main/java/DeepslateGeneration/DeepslateGeneration/BlockFormListener.java
@@ -12,39 +12,49 @@ import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class BlockFormListener implements Listener {
 
-    private Main main;
+    private Main m;
 
-    public BlockFormListener(Main main) {
-        this.main = main;
+    public BlockFormListener(Main m) {
+        this.m = m;
     }
 
     @EventHandler
-    public void onBlockForm(BlockFormEvent e) {
-
+    public boolean onBlockForm(BlockFormEvent e) {
+        boolean changeToDeepslate = false;
         Block b = e.getBlock();
-        //Bukkit.broadcastMessage(e.getNewState().getType().name() + " new state material");
-        if (b.getType().equals(Material.STONE) || b.getType().equals(Material.COBBLESTONE)) {
-        }
-        if (b.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
-            if (b.getY() < 0) {
-                if(e.getNewState().getType().equals(Material.STONE)||e.getNewState().getType().equals(Material.COBBLESTONE))
-                {
-                    e.setCancelled(true);
-                    //Bukkit.broadcastMessage("Canceling old event and creatung new.");
-                    b.setType(Material.DEEPSLATE);
 
-                    /*
-                    //Dunno why, but Calling a new event didn't work.
-                    BlockState bs = e.getNewState();
-                    bs.setType(Material.DEEPSLATE);
-                    BlockFormEvent newEvent = new BlockFormEvent(b,bs);
-                    Bukkit.getServer().getPluginManager().callEvent(newEvent);
-                    */
-                }
+        //Check if Blocktransform is eligable
+        if (!(e.getNewState().getType().equals(Material.STONE) || e.getNewState().getType().equals(Material.COBBLESTONE))) {
+            return false;
+        }
+
+        //Check for if block below is deepslate
+        if (m.GetFORMABOVEDEEPSLATE()) {
+            Location b_loc = b.getLocation().clone();
+            b_loc.setY(b_loc.getY() - 1);
+            Block below = b_loc.getBlock();
+            if (below.getType().equals(Material.DEEPSLATE)) {
+                changeToDeepslate = true;
             }
         }
+
+        //check for height
+        if (b.getY() < m.GetFORMBELOW0()) {
+            changeToDeepslate = true;
+        }
+
+        //if one of previous checks was valid, cancel event and make it deepslate
+        if(changeToDeepslate)
+        {
+            e.setCancelled(true);
+            b.setType(Material.DEEPSLATE);
+            return true;
+        }
+        return false;
     }
 }
+
--- src/main/java/DeepslateGeneration/DeepslateGeneration/Main.java
+++ src/main/java/DeepslateGeneration/DeepslateGeneration/Main.java
@@ -4,10 +4,28 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
-
+    public final String FORMABOVEDEEPSLATE="Create when Deepslate below forming block";
+    public final String FORMBELOW0= "Create when the forming block is below";
     @Override
     public void onEnable() {
+        loadConfig();
         Bukkit.getPluginManager().registerEvents(new BlockFormListener(this), this);
+    }
+
+    public void loadConfig() {
+        getConfig().addDefault(FORMABOVEDEEPSLATE, true);
+        getConfig().addDefault(FORMBELOW0, 0);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public boolean GetFORMABOVEDEEPSLATE () {
+        reloadConfig();
+        return getConfig().getBoolean(FORMABOVEDEEPSLATE);
+    }
+    public int GetFORMBELOW0 () {
+        reloadConfig();
+        return getConfig().getInt(FORMBELOW0);
     }
 
     @Override
